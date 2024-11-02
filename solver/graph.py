85f5c25bbda66e404b42038ebb46e879146cb4c1
Hutanu Eusebiu-Ciprian <eusebiuciprian.hutanu03@gmail.com>
2024-11-02 18:50:02 +0200

    Tracking objects with dictionaries
    Added dictionaries for every object type, from hashed id to the object.

--- solver/graph.py
+++ solver/graph.py
@@ -6,6 +6,11 @@ class Graph:
     id_hashmap: dict[str, int] = {}
     adjacency_list: List[List[int]] = []
 
+    refineries_dict: dict[int, Refinery] = {}
+    tanks_dict: dict[int, Tank] = {}
+    customers_dict: dict[int, Customer] = {}
+    connections_dict: dict[(int, int), Connection] = {}
+
     @staticmethod
     def load_objects(objects):
         for object in objects:
@@ -16,12 +21,18 @@ class Graph:
     def load_data():
         refineries = read_refineries("../data/refineries.csv")
         Graph.load_objects(refineries)
+        for refinery in refineries:
+            Graph.refineries_dict[Graph.id_hashmap[refinery.id]] = refinery
 
         tanks = read_tanks("../data/tanks.csv")
         Graph.load_objects(tanks)
+        for tank in tanks:
+            Graph.tanks_dict[Graph.id_hashmap[tank.id]] = tank
 
         customers = read_customers("../data/customers.csv")
         Graph.load_objects(customers)
+        for customer in customers:
+            Graph.customers_dict[Graph.id_hashmap[customer.id]] = customer
 
         for _ in Graph.mapping_list:
             Graph.adjacency_list.append([])
@@ -29,6 +40,6 @@ class Graph:
         connections = read_connections("../data/connections.csv")
         for connection in connections:
             Graph.adjacency_list[Graph.id_hashmap[connection['from_id']]].append(Graph.id_hashmap[connection['to_id']])
-
+            Graph.connections_dict[(Graph.id_hashmap[connection['from_id']], Graph.id_hashmap[connection['to_id']])] = connection
 
 
